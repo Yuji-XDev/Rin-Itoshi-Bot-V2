5a6112b7453bf341bd361db3dd97224643485046
the-27 <blackoficial2025@gmail.com>
2025-07-05 12:31:44 -0500

    𝖚𝖕𝖉𝖆𝖙𝖊 𝖙𝖎𝖓 𝖎𝖙𝖔𝖘𝖍𝖎

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -20,19 +20,28 @@ let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 
-let texto = `${emoji} *${packname}*
-🚀 *᥎ᥱᥣ᥆ᥴіძᥲძ:*
-→ ${latensi.toFixed(4)}
+let texto = `*${emoji}「 ${packname} 」*
 
-🕒 *Aᥴ𝗍і᥎᥆ Dᥙrᥲᥒ𝗍ᥱ:*
-→ ${muptime}
+╔═════🌟 𝙀𝙎𝙏𝘼𝘿𝙊 🌟════╗
+┃🚀 *⧼ 𝖁𝖊𝖑𝖔𝖈𝖎𝖉𝖆𝖉 ⧽* 
+┃⤷ ⚡ ${latensi.toFixed(4)} ms
+╚════════════════════╝
 
-❄️ *Cһᥲ𝗍s:*
-→ ${chats.length} *Chats Privados*
-→ ${groups.length} *Grupos*
+╔═════⏳ 𝘼𝘾𝙏𝙄𝙑𝙄𝘿𝘼𝘿 ⏳═════╗
+┃🕒 *𝑇𝑖𝑒𝑚𝑝𝑜 𝑒𝑛 𝑙𝑖́𝑛𝑒𝑎:* 
+┃⤷ 🔄 ${muptime}
+╚═══════════════════════╝
 
-🏆 *sᥱr᥎іძ᥆r:*
-➤ *Rᥲm ⪼* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
+╔══════📬 𝘾𝙃𝘼𝙏𝙎 📬══════╗
+┃💬 *𝘾𝙝𝙖𝙩𝙨 𝘿𝙞𝙨𝙥𝙤𝙣𝙞𝙗𝙡𝙚𝙨:*
+┃⤷ 💌 ${chats.length} 𝘾𝙝𝙖𝙩𝙨 𝙋𝙧𝙞𝙫𝙖𝙙𝙤𝙨  
+┃⤷ 👥 ${groups.length} 𝙂𝙧𝙪𝙥𝙤𝙨
+╚══════════════════════╝
+
+╔═════🖥️ 𝙎𝙀𝙍𝙑𝙄𝘿𝙊𝙍 🖥️══════╗
+┃📊 *ℝ𝔸𝕄 𝕌𝕤𝕒𝕕𝕒 / 𝕋𝕠𝕥𝕒𝕝:*
+┃⤷ 💽 ${format(totalmem() - freemem())} / ${format(totalmem())}
+╚═══════════════════════╝`.trim()
 
 m.react('✈️')
 
