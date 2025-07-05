0749f2c7dcfd18aa7670e77aecf7754ffe660275
the-27 <blackoficial2025@gmail.com>
2025-07-05 06:13:49 -0500

    𝖚𝖕𝖉𝖆𝖙𝖊 𝖙𝖎𝖓 𝖎𝖙𝖔𝖘𝖍𝖎

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -128,7 +128,7 @@ export async function before(m, { conn }) {
 
     await conn.sendMessage(m.chat, {
       text: mensaje,
-      footer: '🌟 Rin Itoshi BOT V²',
+      footer: '🧸 ʀɪɴ ɪᴛᴏsʜɪ ʙᴏᴛ 🧸',
       buttons: [
         {
           buttonId: '.menu',
