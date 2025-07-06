896169529029eaacdd208e98dda91e70e2bd59df
the-27 <blackoficial2025@gmail.com>
2025-07-06 08:37:28 -0500

    𝖗𝖎𝖓 𝖎𝖙𝖔𝖘𝖍𝖎

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -107,7 +107,27 @@ global.icono = [
 
 /*global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}*/
 
-global.rcanal = {contextInfo: {forwardingScore: 2025, isForwarded: true, externalAdReply: {title: packname, body: dev, sourceUrl: redes, thumbnailUrl: icono}}}
+global.rcanal = {
+  contextInfo: {
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: channelRD.id,
+      serverMessageId: 100,
+      newsletterName: channelRD.name,
+    },
+    externalAdReply: {
+      showAdAttribution: true,
+      title: packname,
+      body: dev,
+      mediaUrl: null,
+      description: null,
+      previewType: "PHOTO",
+      thumbnailUrl: icono,
+      sourceUrl: redes,
+      mediaType: 1,
+      renderLargerThumbnail: true,
+    },
+  },
 }
 
 
