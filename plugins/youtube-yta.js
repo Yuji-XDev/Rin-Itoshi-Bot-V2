6e9477d2cf74c2a780d77127817f684bd7ea7b37
the-27 <blackoficial2025@gmail.com>
2025-07-08 23:03:17 -0500

    𝒖𝒑𝒅𝒂𝒕𝒆 𝒔𝒖𝒌𝒖𝒏𝒂 𝑴𝑫

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+/*import fetch from "node-fetch";
 import yts from "yt-search";
 
 const encodedApi = "aHR0cHM6Ly9hcGkudnJlZGVuLndlYi5pZC9hcGkveXRtcDM=";
@@ -68,5 +68,66 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.command = ['yta'];
 handler.help = ['yta'];
 handler.tags = ['descargas'];
+
+export default handler;*/
+
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, command, usedPrefix}) => {
+  const text = args.join(" ");
+  if (!text) {
+    return m.reply(
+      `🎧 *Uso correcto del comando:*\n> ${usedPrefix + command} shakira soltera`
+);
+}
+  await m.react('⌛');
+
+  try {
+    const res = await fetch(`https://api.nekorinn.my.id/downloader/spotifyplay?q=${encodeURIComponent(text)}`);
+    const json = await res.json();
+
+    if (!json.status ||!json.result?.downloadUrl) {
+      return m.reply(
+        `❌ *No se encontró resultado para:* ${text}`
+);
+}
+
+    const { title, artist, duration, cover, url} = json.result.metadata;
+    const audio = json.result.downloadUrl;
+
+    await conn.sendMessage(m.chat, {
+      image: { url: cover},
+      caption: `╭─⬣「 *Descargador* 」⬣
+│ ≡◦ 🌴 *Título:* ${title}
+│ ≡◦ 🌲 *Artista:* ${artist}
+│ ≡◦ ⏱️ *Duración:* ${duration}
+│ ≡◦ 🌪️ *Spotify:* ${url}
+╰─⬣`
+}, { quoted: m});
+
+    await conn.sendMessage(m.chat, {
+      audio: { url: audio},
+      mimetype: 'audio/mp4',
+      ptt: false,
+      fileName: `${title}.mp3`
+}, { quoted: fkontak});
+
+    await m.react('✅');
+
+} catch (e) {
+    console.error(e);
+    return m.reply(
+      `╭─⬣「 *Sukuna Bot MD* 」⬣
+│ ≡◦ ⚠️ *Error al procesar la solicitud.*
+│ ≡◦ Intenta nuevamente más tarde.
+╰─⬣`
+);
+}
+};
+
+handler.help = ['yta','play1'];
+handler.tags = ['descargas'];
+handler.command = ['yta','play1']
+handler.register = true;
 
 export default handler;
