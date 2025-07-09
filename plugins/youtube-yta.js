f207a35ac9dfd48994c79ed378b48a9013645f92
the-27 <blackoficial2025@gmail.com>
2025-07-09 13:40:27 -0500

    𝒖𝒑𝒅𝒂𝒕𝒆 𝒔𝒖𝒌𝒖𝒏𝒂 𝑴𝑫

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,4 +1,4 @@
-/*// codigo creado por Black.ofc
+// codigo creado por Black.ofc
 // no robes creaditos, XD
 
 import fetch from 'node-fetch';
@@ -35,72 +35,6 @@ const handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
     await conn.sendMessage(m.chat, { audio: { url: downloadUrl }, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: fkontak });
 
-  } catch (e) {
-    console.error(e);
-    m.reply('⚠️ Ocurrió un error al procesar el video.');
-  }
-};
-
-handler.help = ['play'].map(v => v + ' <texto|url>');
-handler.tags = ['downloader'];
-handler.command = ['play'];
-
-export default handler;
-*/
-
-
-import fetch from 'node-fetch';
-
-const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!text) return m.reply(`*🌲 Ingresa el nombre o link de un video de YouTube.*`);
-
-  try {
-    const api = `https://api.nekorinn.my.id/downloader/ytplay?q=${encodeURIComponent(text)}`;
-    const res = await fetch(api);
-    const json = await res.json();
-
-    if (!json.status || !json.result || !json.result.downloadUrl) {
-      return m.reply('❌ No se pudo obtener el video. Intenta con otro título o revisa la API.');
-    }
-
-    const { title, channel, duration, cover, url } = json.result.metadata;
-    const downloadUrl = json.result.downloadUrl;
-    const thumb = (await conn.getFile(thumbnail))?.data;
-
-    await conn.sendMessage(m.chat, {
-      image: { url: cover },
-      caption: `➤ 🌴 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}\n➤ 🌪️ *Canal:* ${channel}\n➤ ⏱ *Duración:* ${duration}\n➤ 🏞️ *Link:* ${url}`,
-      contextInfo: {
-        externalAdReply: {
-          title: title,
-          body: '☄️ DESCARGAS - PLAY ⛩️',
-          thumbnailUrl: cover,
-          sourceUrl: url,
-          mediaType: 1,
-          renderLargerThumbnail: true
-        }
-      }
-    }, { quoted: m });
-    
-    
-    await conn.sendMessage(m.chat, {
-        audio: { url: downloadUrl },
-        mimetype: "audio/mpeg",
-        ptt: false,
-        contextInfo: {
-          externalAdReply: {
-            title: title,
-            body: 'YouTube - MP3',
-            mediaUrl: url,
-            sourceUrl: url,
-            thumbnail: thumb,
-            mediaType: 1,
-            renderLargerThumbnail: true
-          }
-        }
-      }, { quoted: m });
-    
-    
   } catch (e) {
     console.error(e);
     m.reply('⚠️ Ocurrió un error al procesar el video.');
