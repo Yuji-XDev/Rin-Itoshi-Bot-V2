d8ee18d30f2b178d8e7d4fad3ac9e0401e705748
the-27 <blackoficial2025@gmail.com>
2025-07-09 11:05:06 -0500

    𝒖𝒑𝒅𝒂𝒕𝒆 𝒔𝒖𝒌𝒖𝒏𝒂 𝑴𝑫

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,65 +1,5 @@
-/*let handler = async (m, { conn }) => {
-  const creatorNumber = '51969214380';
-  const creatorName = '⚡ THE BLACK 🍁';
-  const github = 'https://github.com/the-27';
-  const imageUrl = 'https://files.catbox.moe/embijg.jpg';
-  const userId = m.sender;
-
-  // 💼 Tarjeta de contacto vCard
-  const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${creatorName};;;
-FN:${creatorName}
-TEL;type=CELL;type=VOICE;waid=${creatorNumber}:${creatorNumber}
-END:VCARD`.trim();
-
-  
-  await conn.sendMessage(m.chat, {
-    contacts: {
-      displayName: creatorName,
-      contacts: [{ vcard }]
-    }
-  }, { quoted: m });
-
-  
-  const text = `
-┏━━『 👑 𝐂𝐑𝐄𝐀𝐃𝐎𝐑 - 𝐁𝐎𝐓 』━━┓
-┃
-┃ 💎 *Nombre:* ${creatorName}
-┃ 📞 *Número:* wa.me/${creatorNumber}
-┃ 🌐 *GitHub:* ${github}
-┃
-┗━━━━━━━━━━━━━━━━━━━━┛
-✨ ᴇɴʟᴀᴄᴇs ᴜ́ᴛɪʟᴇs • ɪɴꜰᴏʀᴍᴀᴄɪᴏ́ɴ`;
-
-  
-  await conn.sendMessage(m.chat, {
-    text,
-    contextInfo: {
-      mentionedJid: [userId],
-      externalAdReply: {
-        title: '✧ Información del creador ✧',
-        body: creatorName,
-        thumbnailUrl: imageUrl,
-        mediaType: 1,
-        showAdAttribution: true,
-        renderLargerThumbnail: true,
-        sourceUrl: `https://wa.me/${creatorNumber}`
-      }
-    }
-  }, { quoted: m });
-};
-
-handler.help = ['creador', 'owner'];
-handler.tags = ['info'];
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
-
-export default handler;*/
-
 // Código creado por Deylin
-// https://github.com/Deylin-eliac 
-// codigo creado para https://github.com/Deylin-eliac/Pikachu-bot 
+// https://github.com/Deylin-eliac  
 // No quites créditos
 
 import PhoneNumber from 'awesome-phonenumber';
@@ -67,12 +7,12 @@ import PhoneNumber from 'awesome-phonenumber';
 let handler = async (m, { conn }) => {
   m.react('🧃');
   
-const imageUrl = 'https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/main/src/IMG-20250613-WA0194.jpg'
-  const numCreador = '50433191934';
+const imageUrl = 'https://files.catbox.moe/y43t02.jpg'
+  const numCreador = '51969214380';
   const ownerJid = numCreador + '@s.whatsapp.net';
-  const name = await conn.getName(ownerJid) || 'Deylin';
-  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `Hola, mucho gusto. Soy Deylin.`;
-  const empresa = 'Deylin - Servicios Tecnológicos';
+  const name = await conn.getName(ownerJid) || 'Black';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `⛩️ BLACK.OFC 👻`;
+  const empresa = '⚡ THE BLACK 🍁';
 
   const vcard = `
 BEGIN:VCARD
@@ -130,4 +70,4 @@ handler.help = ['owner'];
 handler.tags = ['main'];
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
-export default handler;
+export default handler; 
